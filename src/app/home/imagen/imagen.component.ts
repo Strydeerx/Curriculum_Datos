a3a7bb2a03098350619192f2d9e2e2f4e516c546
janet110 <166758956+janet110@users.noreply.github.com>
2024-04-18 13:38:32 -0600

    Add files via upload

--- src/app/home/imagen/imagen.component.ts
+++ src/app/home/imagen/imagen.component.ts
@@ -1,5 +1,3 @@
-// imagen.component.ts
-
 import { Component } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
@@ -25,26 +23,9 @@ export class ImagenComponent {
       reader.readAsDataURL(file);
     }
   }
-
   onSubmit() {
-    // Aquí puedes implementar la lógica para enviar la foto al servidor
     if (this.imageUrl && this.fileName) {
       console.log('Nombre del archivo:', this.fileName);
-      // Aquí puedes obtener el archivo de imagen como blob
-      // const imageBlob = this.dataURItoBlob(this.imageUrl.changingThisBreaksApplicationSecurity);
-      // Luego puedes enviar el archivo de imagen y el nombre al servidor
     }
   }
-
-  // Función para convertir una URL de datos en un Blob (si es necesario)
-  // dataURItoBlob(dataURI: string): Blob {
-  //   const byteString = atob(dataURI.split(',')[1]);
-  //   const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-  //   const ab = new ArrayBuffer(byteString.length);
-  //   const ia = new Uint8Array(ab);
-  //   for (let i = 0; i < byteString.length; i++) {
-  //     ia[i] = byteString.charCodeAt(i);
-  //   }
-  //   return new Blob([ab], { type: mimeString });
-  // }
 }
