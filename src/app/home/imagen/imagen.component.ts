50a63becb3dfe0df179fea05a60540dbb70a9193
Yiro250 <167122636+Yiro250@users.noreply.github.com>
2024-04-18 16:51:12 -0600

    Add files via upload

--- src/app/home/imagen/imagen.component.ts
+++ src/app/home/imagen/imagen.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
-
 @Component({
   selector: 'app-imagen',
   templateUrl: './imagen.component.html',
   styleUrls: ['./imagen.component.scss'],
 })
-export class ImagenComponent {
+export class ImagenComponent  implements OnInit {
+  perfilForm: FormGroup;
   fileName: string = '';
   imageUrl: SafeUrl | null = null;
 
-  constructor(private sanitizer: DomSanitizer) {}
+  constructor(private sanitizer: DomSanitizer, private fb: FormBuilder) {
+this.perfilForm=fb.group({
 
+  name:['',[Validators.required,Validators.pattern('[A-Za-z]+')]],
+  ApellidP:['',[Validators.required,Validators.pattern('[A-Za-z]+')]],
+  ApellidM:['',[Validators.required,Validators.pattern('[A-Za-z]+')]],
+  Email:['', [Validators.required,Validators.email, Validators.pattern('[a-z0-9]+@gmail.com')]],
+  Phone: ['',[Validators.required, Validators.pattern('[0-9]+')] ],
+  PhoneH: ['', [Validators.required, Validators.pattern('[0-9]+')]],
+
+
+});
+
+   }
   onFileSelected(event: any) {
     const file: File = event.target.files[0];
     if (file) {
@@ -28,4 +41,7 @@ export class ImagenComponent {
       console.log('Nombre del archivo:', this.fileName);
     }
   }
+
+  ngOnInit() {}
+
 }
