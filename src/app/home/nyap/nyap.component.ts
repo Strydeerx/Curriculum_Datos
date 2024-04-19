78940106ece69a03971f6b9877bc2b1620191643
DanielaGarcia1223 <167114057+DanielaGarcia1223@users.noreply.github.com>
2024-04-19 08:42:24 -0600

    Add files via upload

--- src/app/home/nyap/nyap.component.ts
+++ src/app/home/nyap/nyap.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormControl , Validators,  FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-nyap',
@@ -6,8 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nyap.component.scss'],
 })
 export class NyapComponent  implements OnInit {
+  ContactForm: FormGroup
 
-  constructor() { }
+  constructor(
+    private fb:FormBuilder
+  ) {this.ContactForm=this.fb.group({
+      conta:['', [Validators.required,Validators.pattern('[0-9]+'),Validators.minLength(10),Validators.maxLength(10)]],
+      conta1:['', [Validators.required,Validators.pattern('[0-9]+'),Validators.minLength(10),Validators.maxLength(10)]],
+      conta2:['', [Validators.required,Validators.pattern('[0-9]+'),Validators.minLength(10),Validators.maxLength(10)]],
+      infoa:['',Validators.pattern('[A-Za-z0-9]+')],
+      numext:['', Validators.required,Validators.pattern('[0-9]+')],
+      numint:['', Validators.required,Validators.pattern('[0-9]+')],
+      numcel:['', [Validators.required,Validators.pattern('[0-9]+'),Validators.minLength(10),Validators.maxLength(10)]],
+      numtel:['', [Validators.required,Validators.pattern('[0-9]+'),Validators.minLength(10),Validators.maxLength(10)]],
+      numcod:['', [Validators.required,Validators.pattern('[0-9]+'),Validators.minLength(5),Validators.maxLength(5)]],
+      
+    })
+   }
 
   ngOnInit() {}
 
